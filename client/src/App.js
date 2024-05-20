99130b9c1fd9a8ff86475ff37f555af0838b6cf1
Parth <128666215+parthpaghdal0@users.noreply.github.com>
2024-05-20 01:55:32 -0700

    feat: add table and style to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import { useEffect, useState } from "react";
+import { TextField, Button, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
 import io from "socket.io-client";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,8 +8,38 @@ const socket = io.connect("http://52.15.144.174:5010");
 //const socket = io.connect("http://localhost:5010");
 
 function ToastBody(data) {
-  return <div className="text-red-500">
-    {JSON.stringify(data)}
+  return <div className="text-red-500 overflow-auto">
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>PHONE1</TableCell>
+            <TableCell>PHONE2</TableCell>
+            <TableCell>PHONE3</TableCell>
+            <TableCell>Service</TableCell>
+            <TableCell>Billing</TableCell>
+            <TableCell>Customer#</TableCell>
+            <TableCell>Address</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, i) => (
+            <TableRow
+              key={i}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell>{row["PHONE1"]}</TableCell>
+              <TableCell>{row["PHONE2"]}</TableCell>
+              <TableCell>{row["PHONE3"]}</TableCell>
+              <TableCell>{row["Service"]}</TableCell>
+              <TableCell>{row["Billing"]}</TableCell>
+              <TableCell>{row["Customer#"]}</TableCell>
+              <TableCell>{row["Address"]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 }
 
@@ -24,7 +55,8 @@ function App() {
 
   useEffect(() => {
     const callback = (msg) => {
-      toast(ToastBody(msg));
+      if (msg.length > 0)
+        toast(ToastBody(msg));
       //setData(prevData => [...prevData, msg]);
     };
 
@@ -54,9 +86,9 @@ function App() {
 
   if (name === "") {
     return <div className="h-screen flex">
-      <div className="m-auto flex">
-        <input placeholder="Enter name here..." value={input} onChange={(e) => setInput(e.target.value)} />
-        <button onClick={login}>LOGIN</button>
+      <div className="m-auto flex gap-4">
+        <TextField variant="outlined" size="small" label="Enter name here..." value={input} onChange={(e) => setInput(e.target.value)} />
+        <Button variant="contained" onClick={login}>LOGIN</Button>
       </div>
     </div>
   }
@@ -65,12 +97,13 @@ function App() {
       <div className="m-4">
         <div className="flex">
           <span className="text-4xl mr-auto">👋 Hi, {name}! Welcome to Phone Lookup System.</span>
-          <button onClick={logout}>LOGOUT</button>
+          <Button variant="contained" onClick={logout}>LOGOUT</Button>
         </div>
-        <ToastContainer 
-          autoClose={3000}
-          hideProgressBar={true}
+        <ToastContainer
+          autoClose={30000}
           position="top-center"
+          className="!w-[1024px]"
+          bodyClassName="max-h-[320px] overflow-auto !m-4 !items-start"
         />
       </div>
     );
